b87812df6852f4a2d97e8caeea63c7f160257484
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-06-21 15:47:56 +0530

    Redux ToolKit Project

---
+++ reduxToolkitToDo/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+
+function App() {
+
+  
+  return (
+    <>
+      <div>Learn about Redux Toolkit</div>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
